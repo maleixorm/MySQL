48fc547260614b7705e0b64726b59c13cd17973b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-09-20 15:24:29 -0300

    Query do curso de PHP e Segurança

---
+++ Alura-MySQL/Cursos/php-na-web2.sql
@@ -0,0 +1,9 @@
+create table users (
+	id int not null auto_increment primary key,
+    email varchar(250) not null,
+    password varchar(250) not null
+);
+
+select * from users;
+
+select * from videos;
